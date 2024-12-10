84f0bbcc41dfea28baefb57212bcb5d0461fd62a
johndalloxx <john.dall@oxx.no>
2024-12-10 01:53:22 +0100

    testing some pretty printing preprocessor

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,11 @@
 open My_lib.Utils
 open My_lib.D6
+open Fmt
 
-(*TSIA*)
+type person = { name : string; age : int } [@@deriving show]
+type knuti = { knuti : string; age : int } [@@deriving show]
 
-let () = main();;
+let () =
+  let alice = { name = "Alice"; age = 30 } in
+  let knuti = { knuti = "knuti"; age = 1337 } in
+  pr "%a\n" pp_person alice; pr "%a\n" pp_knuti knuti;
